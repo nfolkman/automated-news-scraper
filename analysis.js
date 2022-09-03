60eb19da1210971f8255934a05649b1f007f88c1
nfolkman <nathanielfolkman@gmail.com>
2022-09-02 18:00:36 -0700

    DB fixes

--- analysis.js
+++ analysis.js
@@ -1,41 +1,34 @@
 const db = require('./data')
-const { MONGO_URI } = db
+
 
 const mongoose = require('mongoose')
 
-mongoose.connect(MONGO_URI,{
+// Impact Articles
+mongoose.connect(db.MONGO_URI_Impacts,{
    useNewUrlParser: true})
    .then(()=> console.log('MongoDB Connected'))
    .catch(err => console.log(err))
 
 
    // pass data from scraper into model
-   const resultAnalysisAndSave = data => {
+   const impactsAnalysis = async data => {
       try{
          const Articles = require('./models/Articles')
          const titleQuery = {title:data.title}
          
-         // const updatingData = {title:data.title, summary:data.summary, source:data.source, date:data.date}
+      //To replace all existing docs you can do this
 
+      await Articles.deleteMany({});
+
+
+      //Then this will create the new documents, thus replacing all documents within the database with the new data :)
+
+      return Articles.create(data)
          
-         // this does nothing ||  I want to replace/update ALL existing docs in DB
-         //                   \/
-        return Articles.updateMany({},{title:data.title, summary:data.summary, source:data.source, date:data.date})
-
-
-
-         // this adds more documents to the DB, on top of the ones that are already there
-         // return Articles.create(data)
-
-
-         // /*** HOW TO BUILD CONDITIONALS TO UPDATE/REPLACE ALL DOCUMENTS IN DB IF DOC COUNT IS GREATER THAN 0? ***/
-         
-
-             
 
       }catch(err) {
          console.error(err)
       }
    }
 
-   module.exports = resultAnalysisAndSave
+   module.exports = impactsAnalysis
--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const resultAnalysisAndSave = require('./analysis')
+const impactsAnalysis = require('./analysis')
 const scraper = require('./scraper')
+
+
+// cron job to automate? Unless chosen server has job scheduling options
 
 scraper()
 .then(data => {
-   resultAnalysisAndSave(data)
+   impactsAnalysis(data)
 })
 .catch(console.error())
