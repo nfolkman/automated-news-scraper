560b28fcd6b63cc72bf04de24ecd12718a8fc706
nfolkman <nathanielfolkman@gmail.com>
2022-09-03 19:21:55 -0700

    last touches before test deploy

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const impactsAnalysis = require('./analysis')
 const scraper = require('./scraper')
+const cron = require('node-cron')
 
 
 // cron job to automate? Unless chosen server has job scheduling options
