ff26b539b2d58598857e52d6cd3c5fd78f3c868d
nfolkman <nathanielfolkman@gmail.com>
2022-09-03 20:05:12 -0700

    edits

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ const cron = require('node-cron')
 
 // cron job to automate? Unless chosen server has job scheduling options
 
-cron.schedule('*/30 * * * *',function execute(){scraper()
+
+cron.schedule('*/30 * * * *',()=> {
+   scraper()
 .then(data => {
    impactsAnalysis(data)
 })
-.catch(console.error())})
+.catch(console.error())
+})
